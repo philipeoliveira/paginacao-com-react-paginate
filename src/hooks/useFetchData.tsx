c6de57fd3bd0d23365681c4d3c0ba320c3759cb6
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-15 19:08:10 -0300

    Add placeholderData and some types

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -5,6 +5,14 @@ interface QueryKeyProps {
    queryKey: [string, number];
 }
 
+interface Post {
+   userId: number;
+   id: number;
+   title: string;
+   body: string;
+   [key: string]: string | number;
+}
+
 const LIMIT_PER_PAGE = 5;
 
 const fetchData = async ({ queryKey }: QueryKeyProps) => {
@@ -14,10 +22,11 @@ const fetchData = async ({ queryKey }: QueryKeyProps) => {
       const response = await axios.get(
          `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${LIMIT_PER_PAGE}`
       );
-      const responseData = response.data;
-      const totalCount = response.headers['x-total-count'];
+      const responseData: Post[] = response.data;
+      const totalCount: number = response.headers['x-total-count'];
+      const totalPages: number = Math.ceil(totalCount / LIMIT_PER_PAGE);
 
-      return { responseData, totalCount };
+      return { responseData, totalCount, totalPages };
    } catch (err) {
       console.error('Erro ao buscar os dados.', err);
       throw err;
@@ -28,7 +37,10 @@ export function useFetchData(currentPage: number) {
    const query = useQuery({
       queryKey: ['posts', currentPage],
       queryFn: fetchData,
-      retry: 1, // nova solicitação automática quando há falha na query
+      retry: 1, // Nova solicitação automática quando há falha na query
+      // Mantenha dados anteriores enquanto busca novos dados
+      placeholderData: (prevData) =>
+         prevData ?? { responseData: [], totalCount: 0, totalPages: 0 },
    });
 
    return query;
